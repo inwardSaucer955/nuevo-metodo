9e59664e9fb29f5905e99044b6e0ea9662f8b5e8
tamal <tamal@LAPTOP-RE0OGQFB>
2024-02-29 23:47:06 -0600

    ejemplo de una calculadora despedazada para el profe alfonso

--- src/main/java/com/mycompany/calculadora/Calculadora.java
+++ src/main/java/com/mycompany/calculadora/Calculadora.java
@@ -3,6 +3,7 @@
  */
 
 package com.mycompany.calculadora;
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -11,12 +12,15 @@ package com.mycompany.calculadora;
 public class Calculadora {
 
     public static void main(String[] args) {
+        int n1 =Integer.parseInt(JOptionPane.showInputDialog("coloca el num1"));
+        int n2 =Integer.parseInt(JOptionPane.showInputDialog("coloca el num2"));
+  
+                
         Operaciones op=new Operaciones();
-        op.leerNumeros();
-        op.sumar();
-        op.restar();
-        op.multiplicar();
-        op.dividir();
+        op.sumar(n1, n2);
+        op.restar(n1, n2);
+        op.multiplicar(n1, n2);
+        op.dividir(n1, n1);
         op.mostrarResultados();
         
     }
--- src/main/java/com/mycompany/calculadora/Operaciones.java
+++ src/main/java/com/mycompany/calculadora/Operaciones.java
@@ -10,9 +10,9 @@ package com.mycompany.calculadora;
  */
 import javax.swing.JOptionPane;
 public class Operaciones {
-    //Atributos
-    int numero1;
-    int numero2;
+    
+//Atributos
+
     int suma;
     int resta;
     int multiplicacion;
@@ -20,23 +20,18 @@ public class Operaciones {
     
     //Métodos 
     
-    public void leerNumeros(){
-        numero1=Integer.parseInt(JOptionPane.showInputDialog("coloca el num1"));
-        numero2=Integer.parseInt(JOptionPane.showInputDialog("coloca el num2"));
-        
-    }
-    public void sumar(){
+    public void sumar(int numero1, int numero2){
        suma =numero1 + numero2;
     }
     
-    public void restar(){
+    public void restar(int numero1, int numero2){
        resta =numero1 - numero2;
     }
-     public void multiplicar(){
+     public void multiplicar(int numero1, int numero2){
        multiplicacion =numero1 * numero2;
     }
     
-    public void dividir(){
+    public void dividir(int numero1, int numero2){
        division =numero1 / numero2;
     }
     public void mostrarResultados(){
